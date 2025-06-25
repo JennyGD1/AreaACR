136af6d16ae36abc269fe789c4fe7537176cf9a9
JennyGD1 <jenniferlima61@gmail.com>
2025-06-25 15:00:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def resultados():
                          rubricas_detalhadas=processador.rubricas_detalhadas)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
